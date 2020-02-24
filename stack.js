b13190488f8148761e930fcd1d5045b73d7eeeaa
Sunny Patel <patel.sunny293@gmail.com>
2020-02-24 08:27:49 -0500

    stack

---
+++ stack.js
@@ -0,0 +1,47 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		const newNode = new Node(val);
+
+		if (!this.top) {
+			this.top = newNode;
+			this.bottom = newNode;
+		} else {
+			const temp = this.top;
+			this.top = newNode;
+			this.top.next = temp;
+		}
+		this.length++;
+		return this.length;
+	}
+
+	pop() {
+		if (!this.top) {
+			return null;
+		}
+		const temp = this.top;
+		if (this.top === this.bottom) {
+			this.bottom = null;
+		}
+
+		this.top = this.top.next;
+		this.length--;
+		return temp.value;
+	}
+
+	size() {
+		return this.length;
+	}
+}
