a0f484157c93755c990288ed9e2f103b73e2ea57
Sunny Patel <patel.sunny293@gmail.com>
2020-02-24 15:10:19 -0500

    more stack and queue problems

---
+++ iterate_across_linked_list_backwards.js
@@ -0,0 +1,90 @@
+//
+// -------
+// Prompt:
+// -------
+//
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
+// in the following format:
+//
+//                             'A -> B -> C -> D'
+//
+// ------------
+// Constraints:
+// ------------
+//
+// (1) Your function must be iterative, not recursive.
+// (2) Your function must consume O(n) space.
+// (3) Employee either a Stack, Queue, or some combination of the two in your
+//     solution. (Implement any data structures you need, as you need them.)
+//
+//
+// -----------
+// Let's code!
+// -----------
+
+function iterateAcrossLinkedListBackwards(linkedList) {
+	const stack = new Stack();
+	let result = '';
+	let currNode = linkedList.head;
+
+	while (currNode !== null) {
+		stack.push(currNode);
+		currNode = currNode.next;
+	}
+
+	while (stack.size() > 0) {
+		if (stack.size() > 1) {
+			result += `${stack.pop().value} -> `;
+		} else {
+			result += `${stack.pop().value}`;
+		}
+	}
+	return result;
+}
+
+class Node {
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;
+	}
+	push(val) {
+		const newNode = new Node(val);
+		if (!this.top) {
+			this.top = newNode;
+			this.bottom = newNode;
+		} else {
+			const temp = this.top;
+			this.top = newNode;
+			this.top.next = temp;
+		}
+		this.length++;
+		return this.length;
+	}
+
+	pop() {
+		if (!this.top) return null;
+
+		const temp = this.top;
+		if (this.top === this.bottom) {
+			this.top = null;
+			this.bottom = null;
+		} else {
+			this.top = this.top.next;
+		}
+		this.length--;
+		return temp.value;
+	}
+
+	size() {
+		return this.length;
+	}
+}
