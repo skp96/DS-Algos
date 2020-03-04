e218fe53f16af49b75695a4760f1b01d3146512e
Sunny Patel <patel.sunny293@gmail.com>
2020-03-04 15:38:20 -0500

    convert sorted array into binary search tree

---
+++ balanced_binary_search_tree.js
@@ -0,0 +1,11 @@
+function getHeight(root) {
+	if (!root) return -1;
+	return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+}
+
+function isBalanced(root) {
+	if (!root) return true;
+	let heightDifference = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+
+	return heightDifference && isBalanced(root.left) && isBalanced(root.right);
+}
