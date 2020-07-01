ee1697acf6fc4a7308628626cd9f23dbecf2bcb9
Sunny Patel <patel.sunny293@gmail.com>
2020-07-01 19:34:26 -0400

    complexity update

--- Dynamic Programming/nth_fibonacci.js
+++ Dynamic Programming/nth_fibonacci.js
@@ -7,6 +7,9 @@ function getNthFibRecur(n, memoize = { 1: 0, 2: 1 }) {
 	}
 }
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
 function getNthFibIter(n) {
 	const fibNumbs = [ 0, 1 ];
 	if (n === 1 || n === 2) return fibNumbs[n - 1];
