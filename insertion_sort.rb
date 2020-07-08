fb9df82edff88ae5b58e69b6fa881fed45771e23
Sunny Patel <patel.sunny293@gmail.com>
2020-07-08 16:33:25 -0400

    insertion sort

---
+++ insertion_sort.rb
@@ -0,0 +1,11 @@
+def insertion_sort(array)
+  (1...array.length).each do |i|
+    j = i
+
+    while (j > 0 && array[j] < array[j - 1])
+      array[j], array[j - 1] = array[j - 1], array[j]
+      j -= 1
+    end
+  end
+  return array
+end
