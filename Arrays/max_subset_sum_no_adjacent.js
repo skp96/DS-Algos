41cd8e75faaa8e355731ad5e6594ed8fdf5c670e
Sunny Patel <patel.sunny293@gmail.com>
2020-07-30 14:07:03 -0400

    max non-adjacent sum

---
+++ Arrays/max_subset_sum_no_adjacent.js
@@ -0,0 +1,13 @@
+function maxSubsetSumNoAdjacent(array) {
+	if (!array.length) return 0;
+	const table = new Array(array.length).fill(0);
+	table[0] = array[0];
+
+	for (let i = 1; i < table.length; i++) {
+		const skipLeft = table[i - 2] === undefined ? 0 : table[i - 2];
+		const sum = skipLeft + array[i];
+		const prevSum = table[i - 1];
+		table[i] = Math.max(sum, prevSum);
+	}
+	return table[table.length - 1];
+}
