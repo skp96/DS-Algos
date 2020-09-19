5b91faa261dfe07682031753994ea13f0668ad17
Sunny Patel <patel.sunny293@gmail.com>
2020-09-19 16:42:52 -0400

    binary search python solution optimized

--- Easy/binary_search.py
+++ Easy/binary_search.py
@@ -1,4 +1,4 @@
-def binarySearch(array, target):
+def binarySearchRecur(array, target):
     if not len(array):
         return -1
 
@@ -6,12 +6,33 @@ def binarySearch(array, target):
     midValue = array[midIdx]
 
     if target < midValue:
-        return binarySearch(array[0:midIdx], target)
+        return binarySearchRecur(array[0:midIdx], target)
     elif target > midValue:
-        searchResult = binarySearch(array[midIdx + 1:], target)
+        searchResult = binarySearchRecur(array[midIdx + 1:], target)
         return -1 if searchResult == -1 else midIdx + searchResult + 1
     else:
         return midIdx
 
 # Time Complexity: O(log(n))
 # Space Complexity: O(n)
+
+
+def binarySearchIter(array, target):
+    return binarySearchIterHelper(array, target, left=0, right=len(array) - 1)
+
+
+def binarySearchIterHelper(array, target, left, right):
+    while left <= right:
+        midIdx = (left + right) / 2
+        midValue = array[midIdx]
+
+        if target == midValue:
+            return midIdx
+        elif target < midValue:
+            right = midIdx - 1
+        else:
+            left = midIdx + 1
+    return -1
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
