8706ac6840987455d8f09b3c8f8bf360d37501f7
Sunny Patel <patel.sunny293@gmail.com>
2020-09-22 17:25:44 -0400

    insertion sort python solution

---
+++ Easy/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+
+        while j > 0 and array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+
+    return array
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
