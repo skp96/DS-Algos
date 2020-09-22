b6461b787c1f90e40284a008aa06c0389ca6f063
Sunny Patel <patel.sunny293@gmail.com>
2020-09-22 17:53:23 -0400

    selection sorty python solution

---
+++ Easy/selection_sort.py
@@ -0,0 +1,17 @@
+def selectionSort(array):
+    currentIdx = 0
+
+    while currentIdx < len(array) - 1:
+        smallestIdx = currentIdx
+
+        for i in range(currentIdx + 1, len(array)):
+            if array[smallestIdx] > array[i]:
+                smallestIdx = i
+
+        array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
+        currentIdx += 1
+
+    return array
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
