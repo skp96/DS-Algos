8bf7ac9ac88b6951d0d7563c5919941577ecfc2e
Sunny Patel <patel.sunny293@gmail.com>
2021-01-29 18:15:35 -0500

    AE problems

--- AE/Arrays/two_number_sum.py
+++ AE/Arrays/two_number_sum.py
@@ -29,3 +29,25 @@ def twoNumberSumOptimized(array, targetSum):
 
     # Time Complexity: O(n)
     # Space Complexity: O(n)
+
+
+def twoNumberSumOptimized2(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+
+    while left < right:
+        num1 = array[left]
+        num2 = array[right]
+
+        if num1 + num2 == targetSum:
+            return [num1, num2]
+        elif num1 + num2 > targetSum:
+            right -= 1
+        else:
+            left +=1
+        
+    return []
+
+    # Time Complexity: O(nlog(n))
+    # Space Complexity: O(1)
