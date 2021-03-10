dd294bbcf895a9110718a826a163063a87f44e0d
Sunny Patel <patel.sunny293@gmail.com>
2021-03-09 19:45:00 -0500

    leetcode problem

---
+++ Leetcode/Trees and Graphs/decode_string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decode_string(self, s):
+        stack = []
+        curr_num = 0
+        curr_string = ''
+
+        for char in s:
+            if char == '[':
+                stack.append(curr_string)
+                stack.append(curr_num)
+                curr_string = ''
+                curr_num = 0
+            elif char == ']':
+                num = stack.pop()
+                prev_string = stack.pop()
+                curr_string = prev_string + (num*curr_string)
+            elif char.isdigit():
+                curr_num = curr_num*10 + int(c)
+            else:
+                curr_string += char
+
+        return curr_string
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
