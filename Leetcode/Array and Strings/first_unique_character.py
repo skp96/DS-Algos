5ee5830a1c6c501fe2669b28d34d492109f99dfb
Sunny Patel <patel.sunny293@gmail.com>
2021-03-16 08:23:38 -0400

    leetcode

--- Leetcode/Array and Strings/first_unique_character.py
+++ Leetcode/Array and Strings/first_unique_character.py
@@ -2,40 +2,15 @@ from collections import Counter
 
 
 class Solution:
-  def first_unique(self, s):
-    if not len(s):
-            return -1
 
-        char_tracker = {}
-        idx_tracker = {}
-        
+    def first_unique(self, s):
+        count = Counter(s)
+
         for idx, char in enumerate(s):
-            if char in char_tracker:
-                char_tracker[char] += 1
-            else:
-                char_tracker[char] = 0
-    
-            if char not in idx_tracker:
-                idx_tracker[char] = idx
-  
-        for key in char_tracker:
-            if char_tracker[key] == 0:
-                return idx_tracker[key]
-        
+            if count[char] == 1:
+                return idx
+
         return -1
-
-# Time Complexity: O(n + n) --> O(n)
-# Space Complexity: O(n + n) --> O(n)
-
-  def first_unique(self, s):
-    count = Counter(s)
-
-    for idx, char in enumerate(s):
-      if count[char] == 1:
-        return idx
-    
-    return -1
 
 # Time Complexity: O(n), where n is the number of characters in the string
 # Space Complexity: O(1) --> english alaphabet has a constant number of letters, 26
-
