11a486dee28df0a2188f1217626a29c4945fe5c9
Sunny Patel <patel.sunny293@gmail.com>
2021-04-22 11:23:26 -0400

    recap

--- Window Sliding Problems/fruits_into_baskets.py
+++ Window Sliding Problems/fruits_into_baskets.py
@@ -10,47 +10,52 @@ Write a function to return the maximum number of fruits in both baskets.
   - Explanation: We can put 3 'B' in one basket and two 'C' in the other basket. This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
 """
 
-def fruits_into_basket_brute(fruits):
-  def is_legit(start, end):
-    unique_tracker = set()
+def fruits_into_baskets_brute(fruits):
+  def less_than_two(start, end):
+    unique_fruits = set()
 
-    for start in range(start, end + 1):
-      fruit = fruits[start]
-      if fruit not in unique_tracker:
-        unique_tracker.add(fruit)
-        if len(unique_tracker) > 2:
-          return False
+    for i in range(start, end+1):
+      fruit = fruits[i]
+      unique_fruits.add(fruit)
+
+      if len(unique_fruits) > 2:
+        return False
+    
     return True
 
-  max_fruits = 0
+  max_num_fruits = 0
 
   for i in range(len(fruits)):
     for j in range(i, len(fruits)):
-      if is_legit(i, j):
-        max_fruits = max(max_fruits, j - i + 1)
+      if less_than_two(i, j):
+        max_num_fruits = max(max_num_fruits, j - i + 1)
   
-  return max_fruits
+  return max_num_fruits
 
 # Time Complexity: O(N^3)
-# Space Complexity: O(1)
+# Space Complexity: O(N)
 
 from collections import defaultdict
-def fruits_into_basket_optimized(fruits):
-  max_fruits = 0
+def fruits_into_baskets_opt(fruits):
   window_start = 0
-  fruit_freq = defaultdict(lambda: 0)
+  max_num_fruits = 0
+  unique_freq = defaultdict(lambda: 0)
 
   for window_end in range(len(fruits)):
     fruit = fruits[window_end]
-    fruit_freq[fruit] += 1
-    
-    while len(fruit_freq) > 2:
-      fruit = fruits[window_start]
-      fruit_freq[fruit] -= 1
-      if fruit_freq[fruit] == 0:
-        del fruit_freq[fruit]
+    unique_freq[fruit] += 1
+
+    while len(unique_freq) > 2:
+      left_fruit = fruits[window_start]
+      unique_freq[left_fruit] -= 1
+      if unique_freq[left_fruit] == 0:
+        del unique_freq[left_fruit]
       window_start += 1
     
-    max_fruits = max(max_fruits, window_end - window_start + 1)
+    max_num_fruits = max(max_num_fruits, window_end - window_start + 1)
   
-  return max_fruits
+  return max_num_fruits
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
