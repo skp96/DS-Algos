b5be2dac72e305f4aca57975a9e254afa7f2b527
Sunny Patel <patel.sunny293@gmail.com>
2021-04-22 10:44:53 -0400

    recap

--- Window Sliding Problems/longest_substring_with_k_distinct.py
+++ Window Sliding Problems/longest_substring_with_k_distinct.py
@@ -8,9 +8,31 @@
   - Output: 5
   - Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
 """
-
+from collections import defaultdict
 def longest_substring_w_k_distinct_brute(str, k):
-  longest_length = 0
+  def is_distinct(start, end):
+    freq = defaultdict(lambda: 0)
+
+    for i in range(start, end + 1):
+      char = str[i]
+      freq[char] += 1
+    
+    return len(freq) <= k 
+  
+  longest_substring = 0
+
+  for idx1 in range(len(str)):
+    for idx2 in range(idx1, len(str)):
+      if is_distinct(idx1, idx2):
+        longest_substring = max(longest_substring, idx2 - idx1 + 1)
+  
+  return longest_substring
+
+# Time Complexity: O(N^3)
+# Space Complexity: O(N)
+
+def longest_substring_w_k_distinct_opt_1(str, k):
+  longest_substring = 0
 
   for i in range(len(str)):
     unique_chars = set()
@@ -21,36 +43,32 @@ def longest_substring_w_k_distinct_brute(str, k):
         if len(unique_chars) > k:
           unique_chars = set()
           break
-      longest_length = max(longest_length, j - i + 1)
-  
-  return longest_length
+      longest_substring = max(longest_substring, j - i + 1)
 
-# Time Complexity: O(N * K), where N is the total number of elements in the array
-# Space Complexity: O(K), will be storing a max of K+1 characters in the set
+  return longest_substring
 
-def longest_substring_w_k_distinct_optimized(str, k):
-  unique_freq_chars = {}
-  window_start = 0
-  longest_length = 0
+# Time Complexity: O(N^2)
+# Space Complexity: O(N)
+
+from collections import defaultdict
+def longest_substring_w_k_distinct_opt_2(str, k):
+  window_start, longest_substring = 0
+  unique_freq = defaultdict(lambda: 0)
 
   for window_end in range(len(str)):
     char = str[window_end]
-    if char not in unique_freq_chars:
-      unique_freq_chars[char] = 0
-    unique_freq_chars[char] += 1
-    
-    while len(unique_freq_chars) > k:
-      left_char = str[window_start]
-      unique_freq_chars[left_char] -= 1
-      if unique_freq_chars[left_char] == 0:
-        del unique_freq_chars[left_char]
+    unique_freq[char] += 1
+
+    while len(unique_freq) > k:
+      char = str[window_start]
+      unique_freq[char] -= 1
+      if unique_freq[char] == 0:
+        del unique_freq[char]
       window_start += 1
-
-    longest_length = max(longest_length, window_end - window_start + 1)
+    
+    longest_substring = max(longest_substring, window_end - window_start + 1)
   
-  return longest_length
+  return longest_substring
 
-# Time Complexity: O(N),the outer for loop runs for all elements, and the inner while loop processes each element only once; therefore, the time complexity of the algorithm will be O(N+N)
-# Space Complexity: O(K), will be storing a max of K+1 characters in the dictionary
-
-
+  # Time Complexity: O(N)
+  # Space Complexity: O(K), where K is the number of distinct characters, technically storing K + 1
