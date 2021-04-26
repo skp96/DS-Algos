fa81ef0efbb2932b974f4615aaf60e78db71ddeb
Sunny Patel <patel.sunny293@gmail.com>
2021-04-26 17:16:56 -0400

    window sliding problem

--- Window Sliding Problems/longestSubarraywOnesAfterReplacement.js
+++ Window Sliding Problems/longestSubarraywOnesAfterReplacement.js
@@ -51,3 +51,6 @@ const lengthOfLongestSubstringOpt = (arr, k) => {
   }
   return maxLength;
 }
+
+// Time Complexity: O(N), where N is the number of elements in the array
+// Space Complexity: O(1)
