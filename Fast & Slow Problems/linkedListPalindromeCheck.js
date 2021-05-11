8e4741aa8457932653e1d4c217bb60fa042c70bb
Sunny Patel <patel.sunny293@gmail.com>
2021-05-11 10:57:00 -0400

    fast and slow problem

---
+++ Fast & Slow Problems/linkedListPalindromeCheck.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(value, next=null){
+    this.value = value;
+    this.next = next;
+  }
+}
+
+
+const is_palindromic_linked_list= function(head) {
+  let slow = head;
+  let fast = head;
+  
+  while ((fast !== null && fast.next !== null)) {
+    fast = fast.next.next
+    slow = slow.next
+  }
+  let secondHalfHead = reverseLinkedList(slow);
+  let copySecondHead = secondHalfHead;
+
+  while ((head !== null && secondHalfHead !== null)) {
+    if (head.value !== secondHalfHead.value) {
+      reverseLinkedList(copySecondHead);
+      return false;
+    }
+    head = head.next
+    secondHalfHead = secondHalfHead.next
+  }
+  reverseLinkedList(copySecondHead);
+  return true;
+};
+
+
+const reverseLinkedList = (head) => {
+  let previousNode = null;
+
+  while (head) {
+    next = head.next
+    head.next = previousNode
+    previousNode = head
+    head = next
+  }
+  return previousNode
+}
