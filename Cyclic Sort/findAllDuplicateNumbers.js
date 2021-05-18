874ee5ef53f9528980546f317dfd7cdb92fbfb47
Sunny Patel <patel.sunny293@gmail.com>
2021-05-18 10:11:41 -0400

    cyclic sort

---
+++ Cyclic Sort/findAllDuplicateNumbers.js
@@ -0,0 +1,16 @@
+const findDuplicate = (nums) => {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] !== i + 1) {
+      j = nums[i] - 1;
+      if (nums[i] !== nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+      } else { // we have found the duplicate
+        return nums[i];
+      }
+    } else {
+      i += 1;
+    }
+  }
+  return -1;
+}
