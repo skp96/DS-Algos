bd1f27c798c6599b043bb7f155dcc28f2e403d4d
Sunny Patel <patel.sunny293@gmail.com>
2021-07-21 18:54:45 -0400

    dfs review

--- Grokking/Tree DFS/allPathForASum.js
+++ Grokking/Tree DFS/allPathForASum.js
@@ -7,7 +7,26 @@ class TreeNode {
 }
 
 const findPaths = (root, sum) => {
+  if (root === null) return [];
+  const allPaths = [];
+
+  findPathsHelper(root, sum, allPaths, currPath = [])
+
+  return allPaths;
 }
 
 const findPathsHelper = (root, sum, allPaths, currPath) => {
+  if (root === null) return;
+
+  currPath.push(root.value);
+
+  if (root.left === null && root.right === null && root.value === sum) {
+    // without creating a new array for currPath, we would be mutating the currPath pushed into allPaths when removing elements below
+    allPaths.push([...currPath])
+  } else {
+    findPathsHelper(root.left, sum - root.value, allPaths, currPath)
+    findPathsHelper(root.right, sum - root.value, allPaths, currPath)
+  }
+  //remove last element from currPath as backtracking to the top so currPath only contains the root node
+  currPath.pop();
 }
--- Grokking/Tree DFS/binaryTreePathSum.js
+++ Grokking/Tree DFS/binaryTreePathSum.js
@@ -9,10 +9,17 @@ class TreeNode {
 
 
 const has_path = function(root, sum) {
+  return hasPathHelper(root, sum)
 };
 
-const hasPathHelper = (root, sum, runningSum) => {
-  
+const hasPathHelper = (root, sum) => {
+  if (root === null) return false;
+
+  if (root.left === null && root.right === null && root.value === sum) return true;
+
+  sum = sum - root.value;
+
+  return hasPathHelper(root.left, sum) || hasPathHelper(root.right, sum);
 }
 
 // Time Complexity: O(N);
