2cedc55d250a7cc43385bf9b5cc7c990be8c15e0
Sunny Patel <patel.sunny293@gmail.com>
2021-07-22 18:08:38 -0400

    algoexpert problems

--- AE/Arrays/longestRange.js
+++ AE/Arrays/longestRange.js
@@ -9,7 +9,7 @@ const longestRangeBrute = (array) => {
 
   for (let windowEnd = 1; windowEnd < array.length; windowEnd++) {
     if (array[windowEnd] !== array[windowEnd - 1]) {
-      if (array[windowEnd] === array[windowEnd] + 1) {
+      if (array[windowEnd] === array[windowEnd - 1] + 1) {
         if (windowEnd - windowStart + 1 > longestRange) {
           longestRange = windowEnd - windowStart + 1;
           values = [array[windowStart], array[windowEnd]];
@@ -24,3 +24,44 @@ const longestRangeBrute = (array) => {
 
 // Time Complexity: O(NlogN);
 // Space Complexity: O(1) or O(N)
+
+const longestRangeOptimized = (array) => {
+  let bestRange = [];
+  let longestLength = 0;
+  const nums = {};
+
+  // store all numbers in hash and mark true, which means unvisited
+  for (const num of array) {
+    nums[num] = true;
+  }
+
+  for (const num of array) {
+    if (!nums[num]) continue; //already visited, so ignore
+    nums[num] = false //set flag to visited 
+
+    let currentLength = 1;
+    let left = num - 1;
+    let right = num + 1;
+
+    while (left in nums) { // as long as num to left is in the hash
+      nums[left] = false //set flag to visited
+      currentLength += 1;
+      left -= 1;
+    }
+
+    while (right in nums) { // as long as num to right is in the hash
+      nums[right] = false; // set flag to visited 
+      currentLength += 1;
+      right += 1;
+    }
+
+    if (currentLength > longestLength) {
+      longestLength = currentLength;
+      bestRange = [left + 1, right - 1];
+    }
+  }
+  return bestRange;
+}
+
+// Time Complexity: O(N);
+// Space Complexity: O(N);
