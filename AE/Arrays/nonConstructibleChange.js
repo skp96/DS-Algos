8b6eafdde6c309f3dda1a9a7a7aeb8b5ac541caa
Sunny Patel <patel.sunny293@gmail.com>
2021-07-30 09:49:39 -0400

    review

--- AE/Arrays/nonConstructibleChange.js
+++ AE/Arrays/nonConstructibleChange.js
@@ -1,13 +1,32 @@
-const nonConstructibleChange = (coins) => {
-  coins.sort((a, b) => a - b);
+/*
+Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you cannot create. The given coins can have any positive integer value and aren't necessarily unique (e.g. you can have multiple coins of the same value).
+
+For example, if given [1,2,5], the minimum amount of change you cannot create is 4. You can make change for 1, 2, 3 (1+2), and 5, but not 4. If no given coins, the minimum amount of change you can't create is 1
+
+Sample Input:
+coins = [5,7,1,1,2,3,22]
+
+Sample Output:
+20
+
+*/
+
+const nonConstructibleChangeNaive = (coins) => {
+  
+  //in search for minimum amount of change that cannot be created, so sort array
+  coins.sort((a,b) => a - b);
+  //track the change created
   let changeCreated = 0;
 
   for (let i = 0; i < coins.length; i++) {
     const coin = coins[i];
 
+    //if the coin value is greater than the change created so far + 1
+    //then we can create change for every value from 1 to changeCreated and the coin value, but not changeCreated + 1
     if (coin > changeCreated + 1) return changeCreated + 1;
+
     changeCreated += coin;
-  }
+  } 
   return changeCreated + 1;
 }
 
--- AE/Arrays/tournamentWinner.js
+++ AE/Arrays/tournamentWinner.js
@@ -1,7 +1,7 @@
 /*
 Teams compete in round robin, where each team faces off against all other teams. Only two teams compete against each other at a time, and for each competition, one team is designated the home team, while the other is the away team. In each competition there's always one winner and one loser; there are no ties. A team recieves 3 points if it wins and 0 points if it loses. The winner of tournament is the team that receives the most amount of points.
 
-Given an array of pairs representing the teams that have competed against each other, and an array containing the results of each competition, write aa function that returns the winner of the tournament. The Competitions array has elements in the form of [homeTeam, awayTeam]. The Results array contains information about the winner of each corresponding competition in the competitions array. For example, results[i] denotes the winner of competitions[i], where a 1 in the results array means that the home team won, and a 0 means that the away team won
+Given an array of pairs representing the teams that have competed against each other, and an array containing the results of each competition, write aa function that returns the winner of the tournament. The Competitions array has elements in the form of [homeTeam, awayTeam]. The Results array contains information about the winner of each corresponding competition in the competitions array. For example, results[i] denotes the winner of competitions[i], where a 1 in the results array means that the home team won, and a 0 means that the away team won.
 
 Guaranteed exactly one team will win, each team will compete against all other teams, and the tournament will always have at least two teams
 
@@ -23,7 +23,30 @@ Python - 6 points
 */
 
 const tournamentWinner = (competitions, results) => {
+  const currentBestTeam = "";
+  const scoreBoard = {[currrentBestTeam]: 0};
+
+  for (let i = 0; i < competitions.length; i++) {
+    const [homeTeam, awayTeam] = competitions[i];
+    const result = results[i];
+
+    const winningTeam = result === 1 ? homeTeam : awayTeam;
+
+    updateScoreBoard(winningTeam, scoreBoard);
+
+    if (scoreBoard[winningTeam] > scoreBoard[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    }
+  }
+  return currentBestTeam;
+}
+
+const updateScoreBoard = (winner, scoreBoard) => {
+  if (!(winner in scoreBoard)) {
+    scoreBoard[winner] = 0
+  }
+  scoreBoard[winner] += 3;
 }
 
 // Time Complexity: O(N);
-// Space Complexity: O(K);
+// Space Complexity: O(K), where K is the number of team
